c67b723b792fa48cf6a62f0ef87b698289327ae4
Sergio Zanchetta <primes2h@gmail.com>
2020-07-07 20:10:08 +0200

    [ADD] first commit, new module membership_receipt

---
+++ membership_receipt/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2020 Sergio Zanchetta (Associazione PNLUG - Gruppo Odoo)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Membership Receipt',
+    'version': '12.0.1.0.0',
+    'category': 'Vertical Association',
+    'summary': 'Enable membership receipt',
+    'author': 'Pordenone Linux User Group (PNLUG), Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA/vertical-association',
+    'license': 'AGPL-3',
+    'depends': [
+        'membership',
+        'account_voucher'],
+    'data': ['views/membership_receipt_views.xml'],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
