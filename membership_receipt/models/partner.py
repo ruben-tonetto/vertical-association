b0e61c82436b584ed8baf15457090de94f8b5403
Ruben Tonetto <ruben.tonetto@gmail.com>
2020-07-07 22:12:43 +0200

    [IMP] create draft receipt

---
+++ membership_receipt/models/partner.py
@@ -0,0 +1,46 @@
+from odoo import models, fields, api
+
+from datetime import date
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError, ValidationError
+
+class Partner(models.Model):
+    _inherit = 'res.partner'
+
+    @api.multi
+    def create_membership_receipt(self, product_id=None, datas=None):
+        """ Create Customer Receipt of Membership for partners.
+        @param datas: datas has dictionary value which consist Id of Membership product and Cost Amount of Membership.
+                      datas = {'membership_product_id': None, 'amount': None}
+        """
+        product_id = product_id or datas.get('membership_product_id')
+        amount = datas.get('amount', 0.0)
+        receipt_list = []
+        product = self.env['product.product'].browse(product_id)
+        for partner in self:
+            addr = partner.address_get(['invoice'])
+            if partner.free_member:
+                raise UserError(_("Partner is a free Member."))
+            if not addr.get('invoice', False):
+                raise UserError(_("Partner doesn't have an address to make the receipt."))
+            receipt = self.env['account.voucher'].create({
+                'partner_id': partner.id,
+                'account_id': partner.property_account_receivable_id.id,
+                'fiscal_position_id': partner.property_account_position_id.id,
+            })
+            line_values = {
+                'product_id': product_id,
+                'price_unit': amount,
+                'receipt_id': receipt.id,
+                'name': product.name_get()[0][1],
+                'account_id': partner.property_account_receivable_id.id,
+            }
+            # create a record in cache, apply onchange then revert back to a dictionnary
+            receipt_line = self.env['account.voucher.line'].new(line_values)
+            receipt_line._onchange_line_details()
+            line_values = receipt_line._convert_to_write({name: receipt_line[name] for name in receipt_line._cache})
+            line_values['price_unit'] = amount
+            receipt.write({'line_ids': [(0, 0, line_values)]})
+            receipt_list.append(receipt.id)
+            #receipt.compute_taxes()
+        return receipt_list
---
+++ membership_receipt/wizard/membership_voucher.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import api, fields, models
+from odoo.addons import decimal_precision as dp
+
+
+class MembershipInvoice(models.TransientModel):
+    _inherit = "membership.invoice"
+
+    @api.multi
+    def membership_receipt(self):
+        if self:
+            datas = {
+                'membership_product_id': self.product_id.id,
+                'amount': self.member_price
+            }
+        receipt_list = self.env['res.partner'].browse(self._context.get('active_ids')).create_membership_receipt(datas=datas)
+
+        search_view_ref = self.env.ref('account_voucher.view_voucher_filter', False)
+        form_view_ref = self.env.ref('account_voucher.view_sale_receipt_form', False)
+        tree_view_ref = self.env.ref('account_voucher.view_voucher_tree', False)
+
+        return  {
+            'domain': [('id', 'in', receipt_list)],
+            'name': 'Membership Receipts',
+            'res_model': 'account.voucher',
+            'type': 'ir.actions.act_window',
+            'views': [(tree_view_ref.id, 'tree'), (form_view_ref.id, 'form')],
+            'search_view_id': search_view_ref and search_view_ref.id,
+        }
